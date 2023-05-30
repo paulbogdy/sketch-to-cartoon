bfb88a96755359d45cf70d83ada13b927c42d3ad
paulbogdy <jurcutpaul492001@gmail.com>
2023-05-30 20:05:57 +0300

    Something

--- experiments/training/trainers/TESTING.py
+++ experiments/training/trainers/TESTING.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 from models.HEDNet import HDENet
 from models.SketchInverter import Encoder, Sketcher
