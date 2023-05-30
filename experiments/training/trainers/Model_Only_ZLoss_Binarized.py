e89dec09052672560880ea91312896fc6baea468
paulbogdy <jurcutpaul492001@gmail.com>
2023-05-30 15:18:19 +0300

    Added path for running in terminal

--- experiments/training/trainers/Model_Only_ZLoss_Binarized.py
+++ experiments/training/trainers/Model_Only_ZLoss_Binarized.py
@@ -1,3 +1,8 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
 from models.HEDNet import HDENet
 from models.SketchInverter import Encoder, Sketcher
 from training.ExperimentFramework import Experiment
